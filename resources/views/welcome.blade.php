ebe8d2e35ebdab3fa769a494a8f9e1a863743391
Fatmoubidou <fatma.elammari@gmail.com>
2019-02-24 14:32:04 +0100

    integration home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="jumbotron py-3">
+  <div class="container">
+    <h1 class="display-3">Hello, world!</h1>
+    <p class="lead">Réalisation d'un mini blog avec le framework LARAVEL 5.</p>
+    <p>Fonctionnalités :</p>
+    <ul>
+       <li>Une page d’accueil affichant les 10 derniers articles de la base de données,</li>
+       <li>Un espace d’administration permettant de gérer les articles (ajout, modification et suppression),</li>
+       <li>Un système d’authentification pour accéder à la page d’administration.</li>
+    </ul>
+  </div>
+</div>
 <div class="container">
+
   <h2>Les 10 derniers articles</h2>
-  
+
     <div class="row">
           @foreach($posts as $post)
           <div class="col-md-4 mb-3">
